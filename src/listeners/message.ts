89433a5d8aabe4549ae740dd64762a257d97fca7
Jonathan Zhao <mac@Macs-MacBook-Pro.local>
2021-01-06 20:42:14 -0800

    added auto msg frac detection

---
+++ src/listeners/message.ts
@@ -0,0 +1,22 @@
+import { Listener } from 'discord-akairo';
+import { Message } from 'discord.js';
+import CustomClient from '../CustomClient';
+import CardPriceCommand from '../commands/cardprice';
+
+const serial_regex = /[0-9]*\/[0-9]*/;
+
+export default class SongChangeListener extends Listener {
+  client: CustomClient;
+
+  constructor() {
+    super('message', {
+        emitter: 'client',
+        event: 'message'
+    });
+  }
+
+  exec(message: Message): void {
+    if (message.author.bot || message.author.system || !serial_regex.test(message.content)) return;
+    this.client.getCommandHandler().runCommand(message, new CardPriceCommand(), { content: message.content });
+  }
+}
